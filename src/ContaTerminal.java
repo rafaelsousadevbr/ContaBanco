162cada2d39a5e9af7ad49a69018eaeb365c8aff
rafaelsousadevbr <rafael@sousalima.dev.br>
2024-05-15 20:57:10 -0300

    Criada a variável auxiliar para entrada de dados do programa

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        try (// Criada a variável auxiliar para entrada de dados do programa
+        Scanner scEntrada = new Scanner(System.in)){}
     }
 }
