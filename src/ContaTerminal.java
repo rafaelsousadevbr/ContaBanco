6373db4fe3a68eb38be35470989263a9de9a82c6
rafaelsousadevbr <rafael@sousalima.dev.br>
2024-05-15 21:06:43 -0300

    Criada as variáveis que irão ser manipuladas no programa ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,6 +4,14 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
         try (// Criada a variável auxiliar para entrada de dados do programa
-        Scanner scEntrada = new Scanner(System.in)){}
+        Scanner scEntrada = new Scanner(System.in)){
+            
+            // Criada as variáveis que irão ser manipuladas no programa ContaTerminal
+            String numeroAgencia;
+            int numeroConta;
+            String nomeCliente;
+            double saldoDaConta;
+            String mensagemFinal = "";
+        }
     }
 }
