e5e25954e2d60659d908def30f0e5314dc65d81b
rafaelsousadevbr <rafael@sousalima.dev.br>
2024-05-15 21:10:39 -0300

    Criada as mensagens para solicitação de dados das contas dos correntistas do banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -12,6 +12,23 @@ public class ContaTerminal {
             String nomeCliente;
             double saldoDaConta;
             String mensagemFinal = "";
+
+             // Criada as mensagens para solicitação de dados das contas dos correntistas do banco
+             System.out.println("Bem-vindo ao Banco RAFADEV-DIO!!!");
+            
+             System.out.println("Por gentileza, digitar o número da Agência Bancária:");
+             numeroAgencia = scEntrada.next();
+                         
+             System.out.println("Agora digite o número da conta:");
+             numeroConta = scEntrada.nextInt();
+             scEntrada.nextLine();
+ 
+             System.out.println("Digite por gentileza o nome do titular da conta:");
+             nomeCliente = scEntrada.nextLine();
+ 
+             System.out.println("Por gentileza, insira o valor do saldo em conta:");
+             saldoDaConta = scEntrada.nextDouble();
+ 
         }
     }
 }
